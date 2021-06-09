11c83ad22f9ea4e929e575faeadbc9498a0554cd
Soraya <soraya.l.g.88@gmail.com>
2021-06-09 18:58:42 +0200

    ADD: initial setup

---
+++ hello-cube/src/js/main.js
@@ -0,0 +1,4 @@
+import * as THREE from "../../node_modules/three/build/three.module.js"
+
+// Get a reference to the container element that will hold our scene
+const container = document.querySelector('#scene-container');
