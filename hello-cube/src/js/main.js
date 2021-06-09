440b60661d185a3f761058f818da33dffec64112
Soraya <soraya.l.g.88@gmail.com>
2021-06-09 19:00:01 +0200

    ADD: create the scene

--- hello-cube/src/js/main.js
+++ hello-cube/src/js/main.js
@@ -2,3 +2,10 @@ import * as THREE from "../../node_modules/three/build/three.module.js"
 
 // Get a reference to the container element that will hold our scene
 const container = document.querySelector('#scene-container');
+
+// create a Scene
+const scene = new THREE.Scene();
+
+// Set the background color
+scene.background = new THREE.Color('skyblue');
+
